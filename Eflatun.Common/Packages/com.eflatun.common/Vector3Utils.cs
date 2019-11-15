eeb7ca86054929bd732fe89e66a149ef18bb4faf
S. Tarık Çetin <cetinsamedtarik@gmail.com>
2019-11-16 02:16:27 +0300

    Added deconstructor extension methods for Vector2 and Vector3

---
+++ Eflatun.Common/Packages/com.eflatun.common/Vector3Utils.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Eflatun.Common
+{
+    public static class Vector3Utils
+    {
+        public static void Deconstruct(this Vector3 v3, out float x, out float y, out float z)
+        {
+            x = v3.x;
+            y = v3.y;
+            z = v3.z;
+        }
+    }
+}
