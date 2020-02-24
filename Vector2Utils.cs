4f2d8ec37ce9983627bf2c10caddac418c76f998
git-upm-publisher <https://github.com/starikcetin/git-upm-publisher-2/>
2020-02-24 21:21:51 +0300

    upm release 0.0.4

---
+++ Vector2Utils.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Eflatun.Common
+{
+    public static class Vector2Utils
+    {
+        public static void Deconstruct(this Vector2 v2, out float x, out float y)
+        {
+            x = v2.x;
+            y = v2.y;
+        }
+    }
+}
